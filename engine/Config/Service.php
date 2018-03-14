18a6698832c1be30e7219c3c37039e72f81d5d06
elferos <elfbox@mail.ru>
2018-03-14 23:10:32 +0300

    lesson -> 16

--- engine/Config/Service.php
+++ engine/Config/Service.php
@@ -3,7 +3,8 @@
 return [
     Engine\Service\Database\Provider::class,
     Engine\Service\Router\Provider::class,
-    Engine\Service\View\Provider::class
+    Engine\Service\View\Provider::class,
+    Engine\Service\Config\Provider::class
 ];
 
 ?>
--- engine/Core/Config/Config.php
+++ engine/Core/Config/Config.php
@@ -2,12 +2,36 @@
 namespace Engine\Core\Config;
 
 class Config{
-    public static function item($key, $group = 'items'){
-
+    public static function item($key, $group = 'main'){
+        $groupItems  = static::file($group);
+        
+        return isset($groupItems[$key]) ? groupItems[$key] : null;
     }
 
+    /**
+     * @param [type] $group
+     * @return void
+     */
     public static function file($group){
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/rovercms/' . mb_strtolower(ENV) . '/Config/' . $group . '.php';
 
+        if(file_exists($path)){
+            $items = require_once $path;
+
+            if(is_array($items)){
+                return $items;
+            }else{
+                throw new \Exception(
+                    sprintf('Config file <strong>%s</strong> is not a valid array.', $path)
+                );
+            }
+        }else{
+            throw new \Exception(
+                sprintf('Cannot load config from file, file <strong>%s</strong> does not exist.', $path)
+            );
+        }
+
+        return false;
     }
 }
 ?>
